6f3e169db51302e01db2a3aef77a870bb7d0703b
Antony Videtta <antonyvidetta1986@gmail.com>
2016-01-12 13:57:25 +1300

    added completed tasks function

--- ActionPlan.js
+++ ActionPlan.js
@@ -9,7 +9,19 @@ if (Meteor.isClient) {
     // Get all of the tasks from the database 
     // & return them to the view
     tasks: function(){
-      return Tasks.find({}, {sort: {createdAt: -1}});
+      if(Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        // Otherwise, return all of the tasks 
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    }, 
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    }, 
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
     }
   }); // Close helper
 
@@ -30,6 +42,9 @@ if (Meteor.isClient) {
       // Clear form once task has been submitted
       event.target.text.value = "";
 
+    }, 
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
     }
   }); // Close events
 
@@ -44,6 +59,10 @@ if (Meteor.isClient) {
     "click .delete": function() {
       Tasks.remove(this._id);
     }
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
   });
 
 }
