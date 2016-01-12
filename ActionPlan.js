37e5ab80ab7634408a9a4b96747f82b9cf561d0d
Antony Videtta <antonyvidetta1986@gmail.com>
2016-01-12 15:39:52 +1300

    security and validation complete

--- ActionPlan.js
+++ ActionPlan.js
@@ -2,7 +2,21 @@
 // Create a collection for tasks in mongodb
 Tasks = new Mongo.Collection('tasks');
 
+if (Meteor.isServer) {
+  // This code only runs on the server
+  Meteor.publish("tasks", function() {
+    return Tasks.find({
+      $or: [
+        { private: {$ne: true} },
+        { owner: this.userId}
+      ]
+    });
+  });
+}
+
 if (Meteor.isClient) {
+  // This code only runs on the client
+  Meteor.subscribe("tasks");
 
   Template.body.helpers({
 
@@ -34,10 +48,8 @@ if (Meteor.isClient) {
       // Insert a task into the collection
       var text = event.target.text.value;
 
-      Tasks.insert({
-        text: text,
-        createdAt: new Date() // current time
-      });
+      // Insert a task into the collection 
+      Meteor.call("addTask", text);
 
       // Clear form once task has been submitted
       event.target.text.value = "";
@@ -48,16 +60,23 @@ if (Meteor.isClient) {
     }
   }); // Close events
 
+  Template.task.helpers({
+    isOwner: function () {
+      return this.owner === Meteor.userId();
+    }
+  });
+
   Template.task.events({
     // Set the checked property to the opposite 
     // of it's current value
     "click .toggle-checked": function() {
-    Tasks.update(this._id, {
-      $set: {checked: ! this.checked}
-    });
+      Meteor.call("setChecked", this._id, ! this.checked);
     }, 
     "click .delete": function() {
-      Tasks.remove(this._id);
+      Meteor.call("deleteTask", this._id);
+    },
+    "click .toggle-private": function () {
+      Meteor.call("setPrivate", this._id, ! this.private);
     }
   });
 
@@ -66,3 +85,50 @@ if (Meteor.isClient) {
   });
 
 }
+
+Meteor.methods({
+  addTask: function (text) {
+    // Make sure the user is logged in before inserting a task
+    if(! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteTask: function (taskId) {
+    var task = Tasks.findOne(taskId);
+    if(task.private && task.owner !== Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+    if(task.owner == Meteor.userId()) {
+      Tasks.remove(taskId);
+    } else {
+      throw new Meteor.Error('not-authorized');
+    }
+    
+  },
+    setChecked: function (taskId, setChecked) {
+    var task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      // If the task is private, make sure only the owner can check it off
+      throw new Meteor.Error("not-authorized");
+    }
+ 
+    Tasks.update(taskId, { $set: { checked: setChecked} });
+   },
+  setPrivate: function (taskId, setToPrivate) {
+    var task = Tasks.findOne(taskId);
+ 
+    // Make sure only the task owner can make a task private
+    if (task.owner !== Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+ 
+    Tasks.update(taskId, { $set: { private: setToPrivate } });
+  }
+});
